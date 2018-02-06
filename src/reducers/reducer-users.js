531dadad4215a394f235b40a956f986039601d67
Ankit Gupta <m1042948@mindtree.com>
2018-02-06 08:44:35 +0530

    redux

---
+++ src/reducers/reducer-users.js
@@ -0,0 +1,25 @@
+export default function () {
+    return [
+        {
+            id: 1,
+            first: "Ankit",
+            last: "Gupta",
+            age: 22,
+            description: "FSE"
+        },
+        {
+            id: 2,
+            first: "Chaitanya",
+            last: "Boyapati",
+            age: 23,
+            description: "FSE"
+        },
+        {
+            id: 3,
+            first: "Yash",
+            last: "Garg",
+            age: 24,
+            description: "FSE"
+        }
+    ]
+}
